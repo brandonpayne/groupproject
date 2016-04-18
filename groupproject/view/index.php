06048171f260756dfa6dea32f2ec11486a49e295
brandonpayne <paynebrandon4@gmail.com>
2016-04-18 09:36:39 -0600

    Added files via upload

--- groupproject/view/index.php
+++ groupproject/view/index.php
@@ -3,13 +3,6 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <?php
-    require('model/database.php');
-$query = 'SELECT *
-          FROM mail
-          ORDER BY name';
-$name = $db->query($query);
-    ?>
     <head>
         <meta charset="utf-8">
         <title>Flex - Free Responsive Theme</title>
@@ -21,13 +14,13 @@ http://www.templatemo.com/tm-406-flex
 -->
         <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
         
-        <link rel="stylesheet" href="view/css/bootstrap.min.css">
-        <link rel="stylesheet" href="view/css/font-awesome.css">
-        <link rel="stylesheet" href="view/css/animate.css">
-        <link rel="stylesheet" href="view/css/templatemo_misc.css">
-        <link rel="stylesheet" href="view/css/templatemo_style.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/font-awesome.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/templatemo_misc.css">
+        <link rel="stylesheet" href="css/templatemo_style.css">
 
-        <script src="view/js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
+        <script src="js/vendor/modernizr-2.6.1-respond-1.1.0.min.js"></script>
     </head>
     <body>
         <!--[if lt IE 7]>
@@ -108,7 +101,7 @@ http://www.templatemo.com/tm-406-flex
             </div> <!-- /.site-slider -->
         </div> <!-- /.site-main -->
 
-
+ 
         <div class="content-section" id="services">
             <div class="container">
                 <div class="row">
@@ -395,6 +388,116 @@ http://www.templatemo.com/tm-406-flex
                 </div> <!-- /.row -->
             </div> <!-- /.container -->
         </div> <!-- /#our-team -->
+		
+		
+		
+		<div class="content-section" id="our-team">
+            <div class="container">
+                <div class="row">
+                    <div class="heading-section col-md-12 text-center">
+                        <h2>Our Team</h2>
+                        <p>Get to know our people and company</p>
+                    </div> <!-- /.heading-section -->
+                </div> <!-- /.row -->
+                <div class="row">
+                    <div class="team-member col-md-3 col-sm-6">
+                        <div class="member-thumb">
+                            <img src="images/logos/adidaslogo.jpg" alt="">
+                            <div class="team-overlay">
+                                <h3>Tracy</h3>
+                                <span>Designer</span>
+                                <ul class="social">
+                                    <li><a href="#" class="fa fa-facebook"></a></li>
+                                    <li><a href="#" class="fa fa-twitter"></a></li>
+                                    <li><a href="#" class="fa fa-linkedin"></a></li>
+                                </ul>
+                            </div> <!-- /.team-overlay -->
+                        </div> <!-- /.member-thumb -->
+                    </div> <!-- /.team-member -->
+                    <div class="team-member col-md-3 col-sm-6">
+                        <div class="member-thumb">
+                            <img src="images/member2.jpg" alt="">
+                            <div class="team-overlay">
+                                <h3>Cindy</h3>
+                                <span>Developer</span>
+                                <ul class="social">
+                                    <li><a href="#" class="fa fa-facebook"></a></li>
+                                    <li><a href="#" class="fa fa-twitter"></a></li>
+                                    <li><a href="#" class="fa fa-linkedin"></a></li>
+                                </ul>
+                            </div> <!-- /.team-overlay -->
+                        </div> <!-- /.member-thumb -->
+                    </div> <!-- /.team-member -->
+                    <div class="team-member col-md-3 col-sm-6">
+                        <div class="member-thumb">
+                            <img src="images/member3.jpg" alt="">
+                            <div class="team-overlay">
+                                <h3>Mary</h3>
+                                <span>Director</span>
+                                <ul class="social">
+                                    <li><a href="#" class="fa fa-facebook"></a></li>
+                                    <li><a href="#" class="fa fa-twitter"></a></li>
+                                    <li><a href="#" class="fa fa-linkedin"></a></li>
+                                </ul>
+                            </div> <!-- /.team-overlay -->
+                        </div> <!-- /.member-thumb -->
+                    </div> <!-- /.team-member -->
+                    <div class="team-member col-md-3 col-sm-6">
+                        <div class="member-thumb">
+                            <img src="images/member4.jpg" alt="">
+                            <div class="team-overlay">
+                                <h3>Linda</h3>
+                                <span>Chief Executive</span>
+                                <ul class="social">
+                                    <li><a href="#" class="fa fa-facebook"></a></li>
+                                    <li><a href="#" class="fa fa-twitter"></a></li>
+                                    <li><a href="#" class="fa fa-linkedin"></a></li>
+                                </ul>
+                            </div> <!-- /.team-overlay -->
+                        </div> <!-- /.member-thumb -->
+                    </div> <!-- /.team-member -->
+                </div> <!-- /.row -->
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <div class="skills-heading">
+                            <h3 class="skills-title">Our web design skills</h3>
+                            <p class="small-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                    </div> <!-- /.col-md-12 -->
+                </div> <!-- /.row -->
+                <div class="row">
+                    <div class="col-md-8 col-sm-6">
+                        <p>Aliquam faucibus in dolor sed vestibulum. Sed adipiscing malesuada luctus. Morbi tincidunt, tellus scelerisque scelerisque scelerisque, sapien dui pretium augue, at consectetur sapien tellus vitae nunc. Ut vitae metus quis nulla cursus adipiscing pretium vel dolor. Fusce lacinia accumsan arcu, quis porttitor nisi tincidunt ut. Nunc malesuada nunc eget nunc sollicitudin posuere. Maecenas vitae tortor quis odio hendrerit sagittis.<br><br>
+						Etiam tincidunt, magna eu elementum tristique, sapien nisl venenatis lacus, nec sagittis lectus dui eget lorem. Donec in tempus mi. Aenean egestas interdum dolor, et mollis lectus consequat. Mauris ullamcorper, felis sit amet gravida malesuada, nisi sem rhoncus massa, non tempor est erat sit amet diam.<!-- spacing for mobile viewing --><br><br>
+						</p>
+                    </div> <!-- /.col-md-8 -->
+                    <div class="col-md-4 col-sm-6">
+                        <ul class="progess-bars">
+                            <li>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="72" aria-valuemin="0" aria-valuemax="100" style="width: 72%;">Photoshop 72%</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">HTML 80%</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100" style="width: 65%;">WordPress 65%</div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="progress">
+                                    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">Marketing 80%</div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div> <!-- /.col-md-4 -->
+                </div> <!-- /.row -->
+            </div> <!-- /.container -->
+        </div> <!-- /#our-team -->
 
 
 
@@ -429,7 +532,7 @@ http://www.templatemo.com/tm-406-flex
                     </div> <!-- /.col-md-7 -->
                     <div class="col-md-5 col-sm-6">
                         <div class="contact-form">
-                            <form action="model/addcontact.php" method="post" name="contactform" id="contactform">
+                            <form method="post" name="contactform" id="contactform">
                                 <p>
                                     <input name="name" type="text" id="name" placeholder="Your Name">
                                 </p>
@@ -440,7 +543,7 @@ http://www.templatemo.com/tm-406-flex
                                     <input name="subject" type="text" id="subject" placeholder="Subject"> 
                                 </p>
                                 <p>
-                                    <textarea name="message" id="comments" placeholder="Message"></textarea>    
+                                    <textarea name="comments" id="comments" placeholder="Message"></textarea>    
                                 </p>
                                 <input type="submit" class="mainBtn" id="submit" value="Send Message">
                             </form>
